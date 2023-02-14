b04db9eca65bebdfbd89a6edcba852f78e55a5cd
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-14 16:14:38 -0300

    Criando métodos de consulta para créditos

--- assets/js/modulos/controller/creditosController.js
+++ assets/js/modulos/controller/creditosController.js
@@ -12,6 +12,26 @@ const creditosController = () => {
   return creditos;
 }
 
+function existeNoNome(string){
+  return creditosConteudo.some(credito => credito.nome.substring(0, string.length).toLowerCase() == string.toLowerCase())
+}
+
+function buscarNoNome(string){
+  return creditosConteudo.filter(credito => credito.nome.substring(0, string.length).toLowerCase() == string.toLowerCase());
+}
+
+function buscarNoNomePorPalavra(string){
+  return creditosConteudo.filter(credito => (credito.nome.split(' ')).find(palavra => palavra.substring(0, string.length).toLowerCase() == string.toLowerCase()));
+}
+
+function buscarNaDescricao(string){
+  return creditosConteudo.filter(credito => credito.descricao.substring(0, string.length).toLowerCase() == string.toLowerCase());
+}
+
+function buscarNaDescricaoPorPalavra(string){
+  return creditosConteudo.filter(credito => (credito.descricao.split(' ')).find(palavra => palavra.substring(0, string.length).toLowerCase() == string.toLowerCase()));
+}
+
 export{
   creditosController
 }
