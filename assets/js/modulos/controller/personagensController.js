8e8714c82d9d23e802e3b808c0b7beeae746d916
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-14 15:59:11 -0300

    Criando métodos de consulta para personagens

--- assets/js/modulos/controller/personagensController.js
+++ assets/js/modulos/controller/personagensController.js
@@ -3,13 +3,33 @@ import { PersonagemModel } from "../model/personagensModel.js";
 
 const personagensController = () => {
   const personagens = new Array();
-
+  
   personagensConteudo.forEach(personagem => {
     const personagemModel = new PersonagemModel(personagem.nome.trim(), personagem.descricao.trim(), personagem.link.trim());
     personagens.push(personagemModel);
   })
-
+  
   return personagens;
+}
+
+function existeNoNome(string){
+  return personagensConteudo.some(personagem => personagem.nome.substring(0, string.length).toLowerCase() == string.toLowerCase())
+}
+
+function buscarNoNome(string){
+  return personagensConteudo.filter(personagem => personagem.nome.substring(0, string.length).toLowerCase() == string.toLowerCase());
+}
+
+function buscarNoNomePorPalavra(string){
+  return personagensConteudo.filter(personagem => (personagem.nome.split(' ')).find(palavra => palavra.substring(0, string.length).toLowerCase() == string.toLowerCase()));
+}
+
+function buscarNaDescricao(string){
+  return personagensConteudo.filter(personagem => personagem.descricao.substring(0, string.length).toLowerCase() == string.toLowerCase());
+}
+
+function buscarNaDescricaoPorPalavra(string){
+  return personagensConteudo.filter(personagem => (personagem.descricao.split(' ')).find(palavra => palavra.substring(0, string.length).toLowerCase() == string.toLowerCase()));
 }
 
 export {
