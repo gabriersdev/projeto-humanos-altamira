889abfbc6fb232e8f9c59b5d5ab0779f246d0de9
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-17 08:59:35 -0300

    Criando controle de volume dropdown e add. dados das trilhas sonoras

---
+++ assets/js/modulos/conteudo/trilhaSonora.js
@@ -0,0 +1,121 @@
+export const trilhaSonora = [
+  {
+    nome: 'Os Meninos de Altamira',
+    link: 'https://youtu.be/kiRQmD5jLmU'
+  },
+
+  {
+    nome: 'Pequenas Vidas',
+    link: 'https://youtu.be/hEQBv6We0GM'
+  },
+  
+  {
+    nome: 'Fim da Infância',
+    link: 'https://youtu.be/V35KQ7rNDbQ'
+  },
+  
+  {
+    nome: 'Conte Tudo de Novo',
+    link: 'https://youtu.be/Pvek68rGr5Q'
+  },
+  
+  {
+    nome: 'A Luta',
+    link: 'https://youtu.be/U4hkQDOBxjE'
+  },
+  
+  {
+    nome: 'Os Poderosos',
+    link: 'https://youtu.be/vyXE0E4Vq8A'
+  },
+
+  {
+    nome: 'Há Algo Macabro',
+    link: 'https://youtu.be/UR39izi_dHs'
+  },
+
+  {
+    nome: 'Cidade em Pânico',
+    link: 'https://youtu.be/tnCC7obPIk4'
+  },
+  
+  {
+    nome: 'Tormenta',
+    link: 'https://youtu.be/NTLWCtmOAXA'
+  },
+  
+  {
+    nome: 'Precisamos nos Unir',
+    link: 'https://youtu.be/EFGaGqalCVU'
+  },
+  
+  {
+    nome: 'Quem São?',
+    link: 'https://youtu.be/TKVHlS-kt70'
+  },
+  
+  {
+    nome: 'O Tempo Que Passa',
+    link: 'https://youtu.be/38TE3wEhy5c'
+  },
+
+  {
+    nome: 'No Meio da Floresta',
+    link: 'https://youtu.be/a-IvbO-_gqk'
+  },
+
+  {
+    nome: 'Xingu',
+    link: 'https://youtu.be/e4vk4WNmkg0'
+  },
+  
+  {
+    nome: 'Esclarecimentos',
+    link: 'https://youtu.be/Y7Oap_S3b8k'
+  },
+  
+  {
+    nome: 'Cortes',
+    link: 'https://youtu.be/DezSChuUuRk'
+  },
+  
+  {
+    nome: 'O Monstro de Altamira',
+    link: 'https://youtu.be/zcpHnIVQHY4'
+  },
+  
+  {
+    nome: 'Um ou Quatro?',
+    link: 'https://youtu.be/-rCBZ4c2nRQ'
+  },
+
+  {
+    nome: 'As Famílias',
+    link: 'https://youtu.be/stod_XIkpfU'
+  },
+
+  {
+    nome: 'Plenitude',
+    link: 'https://youtu.be/XyOO8bpQZkI'
+  },
+  
+  {
+    nome: 'Superar Tudo',
+    link: 'https://youtu.be/-l_F8WCR7h4'
+  },
+  
+  {
+    nome: 'Foi Assim',
+    link: 'https://youtu.be/iWQLDKkB7is'
+  },
+  
+  {
+    nome: 'Memória',
+    link: 'https://youtu.be/_RfoPb9897Y'
+  },
+  
+  {
+    nome: 'Transamazônica',
+    link: 'https://youtu.be/OKsePWgQ4bU'
+  }
+]
